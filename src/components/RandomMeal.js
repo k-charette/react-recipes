04a047db80861c2c3fe6f6fa2ed06198a5640d90
Ken Charette <charettekenneth@gmail.com>
2020-06-07 20:09:01 -0400

    Fetching random meal data from TheMealDB API

--- src/components/RandomMeal.js
+++ src/components/RandomMeal.js
@@ -6,27 +6,37 @@ const RandomMeal = () => {
     const [meal, setMeal] = useState(undefined)
 
     useEffect(() => {
-        fetch(API_URL)
-        .then((response) => {
-            if(response.ok){
-                return response
-            } else {
-                let errorMessage = `${response.status} ${response.statusText}`,
-                    error = new Error(errorMessage)
-                    throw(error)
-            }
-        })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data)
-        })
+        // fetch(API_URL)
+        // .then((response) => {
+        //     if(response.ok){
+        //         return response
+        //     } else {
+        //         let errorMessage = `${response.status} ${response.statusText}`,
+        //             error = new Error(errorMessage)
+        //             throw(error)
+        //     }
+        // })
+        // .then((response) => response.json())
+        // .then((data) => {
+        //     setMeal(data.meals[0])
+        // })
+
+        async function getMeal() {
+            const res = await fetch(API_URL)
+            const data = await res.json()
+            setMeal(data.meals[0])
+        }
+
+        getMeal()
     }, [])
+
+    console.log(meal)
 
     if (!meal) return null
 
     return (
         <div>
-            <h1> Random Meal </h1>
+            <h1>{meal.strMeal}</h1>
         </div>
     )
 }
